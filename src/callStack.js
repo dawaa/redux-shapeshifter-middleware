551a1efebe20f7401210fdff33fb705732393904
Alejandro Andersson <alejandro@thelad.io>
2018-11-19 15:09:48 +0100

    Convert `Array.find()` to ES5 compatible solution
    IE11 still doesn't provide support for `Array.prototype.find()` and therefore I
    had to change solution to something ES5 friendly.
    
    This also to avoid having to add `babel-polyfill` just to polyfill one method.

--- src/callStack.js
+++ src/callStack.js
@@ -1,7 +1,8 @@
 const callStack = []
 
 export function existsInStack(call) {
-  return callStack.find(x => x.call === call) || false
+  const index = callStack.map( x => x.call ).indexOf( call )
+  return index >= 0 ? callStack[ index ] : false
 }
 
 export function addToStack(call) {
@@ -12,14 +13,16 @@ export function removeFromStack(call) {
   let index;
   let found = false;
 
-  callStack.find((x, i) => {
-    if ( x.call === call && found === false ) {
+  for (let i=0; i<callStack.length; i++) {
+    const item = callStack[ i ]
+    if ( item.call === call && found === false ) {
       found = true
-      return index = i
+      index = i
+      break
     }
 
     index = null
-  })
+  }
 
   if ( index != null ) {
     callStack.splice( index, 1 )
